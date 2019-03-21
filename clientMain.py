55c63eabdd6ad24b5c70bc5b3c6b3ae8abd17aeb
sagun101 <sagunme80@gmail.com>
2019-03-21 18:11:24 -0400

    client collision detection

--- clientMain.py
+++ clientMain.py
@@ -136,6 +136,7 @@ def main(conn):
             window.blit(brick,(int((WIDTH/2)-30),int(HEIGHT -90-60*(i+1))))
         if state == 1:
             font(window,time,WIDTH/2,HEIGHT/2,80)
+            fire = False
         elif state == 2:
             font(window,time,WIDTH/2,30)
             if fire == False and player.destroy == False:
@@ -157,20 +158,39 @@ def main(conn):
         elif state == 3:
             font(window,time,WIDTH/2,HEIGHT/2,80)
         elif state == 4:
-            #update
+            if player.bullet != None:          
+                player.bullet.update(dt)
             for i in ally.keys():
                temp = ally[i]
-               if temp.bullet != None:  #  !=
-                   
-                   temp.bullet.update(dt)
+               if temp.bullet != None:
                    temp.bullet.draw(window)
-                
             for i in enemy.keys():
                 temp = enemy[i]
-                if temp.bullet is not None:
-                   temp.bullet.update(dt)
+                if temp.bullet != None:
                    temp.bullet.draw(window)
-                
+            #collision detection
+            if player.bullet != None:
+                if player.bullet.box.colliderect(screen) == False:
+                    player.bullet.destroy = True
+                if player.bullet.box.colliderect(brick_collider) == True:
+                    player.bullet.destroy = True
+                if player.bullet.box.colliderect(ground.get_rect(x =0, y = int(HEIGHT -110))) == True:
+                    player.bullet.destroy = True
+                for i in ally.keys():
+                    if player.bullet.box.colliderect(ally[i].box) == True:
+                        player.bullet.destroy = True
+                    if ally[i].bullet.box.colliderect(player.box) == True and ally[i].bullet.box != None:
+                        player.hp -= ally[i].bullet.dmg
+                for i in enemy.keys():
+                    if player.bullet.box.colliderect(enemy[i].box) ==True:
+                        player.bullet.destroy = True
+                    if enemy[i].bullet.box.colliderect(player.box) == True and enemy[i].bullet !=None:
+                        player.hp -= enemy[i].bullet.dmg
+                        
+                if player.bullet.destroy == True:
+                    player.bullet = None
+            if player.hp <= 0:
+                player.destroy = True
             
         if state == 2 and fire == False:
             window.blit(fireButton[0],(int(WIDTH/2 - 72),int(HEIGHT -65)))
